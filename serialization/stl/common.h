7952c2a52beaa7c7a014a2b4e829519776e18a6d
luiz <luizcostaalcantara@gmail.com>
2026-05-08 13:19:28 -0300

    Termux upload: serialization/stl/common.h

---
+++ serialization/stl/common.h
@@ -0,0 +1,52 @@
+/*
+MIT License
+
+Copyright (c) 2026 Attila Csikós
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#pragma once
+
+#include "../nvp.h"
+#include "../traits.h"
+#include <cstddef>
+#include <utility>
+#include <variant>
+
+namespace OrthoTree
+{
+  template<typename TArchive, typename T1, typename T2>
+  auto serialize(TArchive& ar, std::pair<T1, T2>& val)
+    -> std::enable_if_t<is_stl_serialization_enabled_v<TArchive>>
+  {
+    ar& make_nvp("key", const_cast<std::remove_const_t<T1>&>(val.first));
+    ar& make_nvp("value", val.second);
+  }
+
+  template<typename TArchive>
+  auto serialize(TArchive&, std::monostate&)
+    -> std::enable_if_t<is_stl_serialization_enabled_v<TArchive>>
+  {}
+
+  template<typename TArchive>
+  auto serialize(TArchive&, std::nullptr_t&)
+    -> std::enable_if_t<is_stl_serialization_enabled_v<TArchive>>
+  {}
+} // namespace OrthoTree
