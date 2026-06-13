4cc0fa95c49a08da9b93df67fa55bf41fbdfb0bb
luiz <luizcostaalcantara@gmail.com>
2026-06-13 11:25:05 -0300

    Termux upload: deepseek_cpp_20260425_f1e0c7.cpp

---
+++ deepseek_cpp_20260425_f1e0c7.cpp
@@ -0,0 +1,11 @@
+// File 44: modules/gaia/register_types.h
+
+#ifndef GAIA_REGISTER_TYPES_H
+#define GAIA_REGISTER_TYPES_H
+
+#include "modules/register_module_types.h"
+
+void initialize_gaia_module(ModuleInitializationLevel p_level);
+void uninitialize_gaia_module(ModuleInitializationLevel p_level);
+
+#endif // GAIA_REGISTER_TYPES_H
