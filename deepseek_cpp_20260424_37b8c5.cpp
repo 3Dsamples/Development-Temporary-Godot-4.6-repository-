21d1d3fdbd20b89973c57cdadbbb7cfc2a798899
luiz <luizcostaalcantara@gmail.com>
2026-06-13 11:24:04 -0300

    Termux upload: deepseek_cpp_20260424_37b8c5.cpp

---
+++ deepseek_cpp_20260424_37b8c5.cpp
@@ -0,0 +1,12 @@
+// genesis/constants.cpp
+
+#include "genesis/constants.h"
+
+// This file is mostly empty because all constants are defined inline in the header.
+// However, we provide a place for any non-inline constant definitions or helper
+// functions that might be added in the future.
+
+namespace genesis {
+namespace constants {
+
+// Explicit instantiations or non-constexpr globals could go here
